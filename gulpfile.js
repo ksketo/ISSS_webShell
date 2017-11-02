c2341bdc97ef7b406f02709f95e11ee15658b067
ksketo <kfaliagkas90@gmail.com>
2017-11-02 23:07:14 +0000

    Lint code with standard.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,59 @@
-var gulp = require('gulp');
-var fs = require('fs');
-var path = require('path');
+var gulp = require('gulp')
+var fs = require('fs')
+var path = require('path')
 
-var clean = require('gulp-clean');
-var handlebars = require('gulp-compile-handlebars');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
-var cleanCSS = require('gulp-clean-css');
-var htmlmin = require('gulp-htmlmin');
+var clean = require('gulp-clean')
+var handlebars = require('gulp-compile-handlebars')
+var rename = require('gulp-rename')
+var uglify = require('gulp-uglify')
+var cleanCSS = require('gulp-clean-css')
+var htmlmin = require('gulp-htmlmin')
 
 gulp.task('clean', function () {
-    return gulp.src('dist/**/*.*', { read: false })
+  return gulp.src('dist/**/*.*', { read: false })
         .pipe(clean({ force: true }))
-        .pipe(gulp.dest('dist'));
-});
+        .pipe(gulp.dest('dist'))
+})
 
 gulp.task('static', function () {
-    return gulp.src('src/static/**/*.*')
-        .pipe(gulp.dest('dist/static/'));
-});
+  return gulp.src('src/static/**/*.*')
+        .pipe(gulp.dest('dist/static/'))
+})
 
 gulp.task('scripts', function () {
-    return gulp.src('src/*.js')
+  return gulp.src('src/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('dist/'));
-});
+        .pipe(gulp.dest('dist/'))
+})
 
 gulp.task('styles', function () {
-    return gulp.src('src/*.css')
+  return gulp.src('src/*.css')
         .pipe(cleanCSS())
-        .pipe(gulp.dest('dist/'));
-});
+        .pipe(gulp.dest('dist/'))
+})
 
 gulp.task('html', function (cb) {
-    fs.readdir(path.join(__dirname, 'src/data/'), function (err, files) {
-        var templateData = {};
-        var options = {
-            ignorePartials: true,
-            batch: ['./src/partials'],
-	    helpers: {
-		command: function (str) {
-		    return new handlebars.Handlebars.SafeString(
-			'<a href="javascript:void(0)" onclick="main.autoType(\'' + str + '\')" title="' + str + '">' + str + '</a>');
-		}
-	    }
-        };
+  fs.readdir(path.join(__dirname, 'src/data/'), function (err, files) {
+    var templateData = {}
+    var options = {
+      ignorePartials: true,
+      batch: ['./src/partials'],
+      helpers: {
+        command: function (str) {
+          return new handlebars.Handlebars.SafeString(
+            '<a href="javascript:void(0)" onclick="main.autoType(\'' + str + '\')" title="' + str + '">' + str + '</a>')
+        }
+      }
+    }
 
-        gulp.src('src/index.hbs')
+    gulp.src('src/index.hbs')
             .pipe(handlebars(templateData, options))
             .pipe(htmlmin({ collapseWhitespace: true }))
             .pipe(rename('index.html'))
-            .pipe(gulp.dest('dist'));
+            .pipe(gulp.dest('dist'))
 
-        cb();
-    });
-});
+    cb()
+  })
+})
 
-gulp.task('default', ['static', 'scripts', 'styles', 'html']);
+gulp.task('default', ['static', 'scripts', 'styles', 'html'])
--- src/script.js
+++ src/script.js
@@ -1,155 +1,151 @@
 var main = new Terminator(document.getElementById('contentWrapper'),
-            {
-                prefix: '<span class="green">kostas</span><span class="red">@</span><span class="yellow">Web</span>:<span class="red">~$</span> ',
-                alwaysFocus: true,
-                autoScroll: (window.innerWidth >= 600)
-            });
+  {
+    prefix: '<span class="green">kostas</span><span class="red">@</span><span class="yellow">Web</span>:<span class="red">~$</span> ',
+    alwaysFocus: true,
+    autoScroll: (window.innerWidth >= 600)
+  })
 
-window.onresize = function() {
-    console.log("Resizing!");
-    if (window.innerWidth >= 600) {
-        main.config.autoScroll = true;
-    } else {
-        main.config.autoScroll = false;
-    }
-};
-
-function printClass(term, className) {
-    var cloneTarget = null;
-    if (className && (cloneTarget = document.getElementsByClassName(className))) {
-        if (cloneTarget[0]) {
-            term.writeHTML(cloneTarget[0].innerHTML);
-        } else {
-            term.writeLine("cat: " + className + ": No such file or directory");
-        }
-    }
+window.onresize = function () {
+  console.log('Resizing!')
+  if (window.innerWidth >= 600) {
+    main.config.autoScroll = true
+  } else {
+    main.config.autoScroll = false
+  }
 }
 
-main.register(function(term, command) {
-    term.writeLine('welcome.txt');
-    term.writeLine('contact.txt');
-    term.writeLine('help.txt');
-    term.writeLine('git.txt');
-    term.prompt();
-}, 'ls');
+function printClass (term, className) {
+  var cloneTarget = null
+  if (className && (cloneTarget = document.getElementsByClassName(className))) {
+    if (cloneTarget[0]) {
+      term.writeHTML(cloneTarget[0].innerHTML)
+    } else {
+      term.writeLine('cat: ' + className + ': No such file or directory')
+    }
+  }
+}
 
-main.register(function(term, command) {
-    command = command.split(' ');
-    var arg = command[1] || '';
-    if (arg.indexOf('.txt') !== -1
-        && arg.indexOf('.txt') === (arg.length - 4))
-        arg = arg.substring(0, arg.indexOf('.txt'));
+main.register(function (term, command) {
+  term.writeLine('welcome.txt')
+  term.writeLine('contact.txt')
+  term.writeLine('help.txt')
+  term.writeLine('git.txt')
+  term.prompt()
+}, 'ls')
 
-    printClass(term, arg);
-    term.prompt();
-    return;
-}, 'cat');
+main.register(function (term, command) {
+  command = command.split(' ')
+  var arg = command[1] || ''
+  if (arg.indexOf('.txt') !== -1 && arg.indexOf('.txt') === (arg.length - 4)) {
+    arg = arg.substring(0, arg.indexOf('.txt'))
+  }
 
-main.register(function(term, command) {
-	printClass(term, 'contact');
-	term.prompt();
-	return;
-}, 'contact');
+  printClass(term, arg)
+  term.prompt()
+}, 'cat')
 
-main.register(function(term, command) {
-	printClass(term, 'help');
-	term.prompt();
-	return;
-}, ['help', 'man']);
+main.register(function (term, command) {
+  printClass(term, 'contact')
+  term.prompt()
+}, 'contact')
 
-main.register(function(term, command) {
-    printClass(term, 'git');
-    term.prompt();
-    return;
-}, 'git');
+main.register(function (term, command) {
+  printClass(term, 'help')
+  term.prompt()
+}, ['help', 'man'])
 
-main.register(function(term, command) {
-    term.writeLine('Redirecting to Github...');
-    setTimeout(function() {
-        window.location = 'https://github.com/ksketo';
-        term.prompt();
-    }, 750);
-}, ['gh', 'github']);
+main.register(function (term, command) {
+  printClass(term, 'git')
+  term.prompt()
+}, 'git')
 
-main.register(function(term, command) {
-    term.writeLine('Redirecting to StackOverflow...');
-    setTimeout(function() {
-        window.location = 'https://stackoverflow.com/users/4619005/kostas';
-        term.prompt();
-    }, 750);
-}, ['so', 'stackoverflow']);
+main.register(function (term, command) {
+  term.writeLine('Redirecting to Github...')
+  setTimeout(function () {
+    window.location = 'https://github.com/ksketo'
+    term.prompt()
+  }, 750)
+}, ['gh', 'github'])
 
-main.register(function(term, command) {
-    term.writeLine('Redirecting to Linkedin...');
-    setTimeout(function() {
-        window.location = 'https://www.linkedin.com/in/konstantinos-faliagkas-6241022a/';
-        term.prompt();
-    }, 750);
-}, ['linkedin']);
+main.register(function (term, command) {
+  term.writeLine('Redirecting to StackOverflow...')
+  setTimeout(function () {
+    window.location = 'https://stackoverflow.com/users/4619005/kostas'
+    term.prompt()
+  }, 750)
+}, ['so', 'stackoverflow'])
 
-main.register(function(term, command) {
-    term.writeLine('Redirecting to Twitter...');
-    setTimeout(function() {
-        window.location = 'https://twitter.com/kwst_f';
-        term.prompt();
-    }, 750);
-}, ['twitter']);
+main.register(function (term, command) {
+  term.writeLine('Redirecting to Linkedin...')
+  setTimeout(function () {
+    window.location = 'https://www.linkedin.com/in/konstantinos-faliagkas-6241022a/'
+    term.prompt()
+  }, 750)
+}, ['linkedin'])
 
-main.register(function(term, command) {
-    term.writeLine('Redirecting to Steem...');
-    setTimeout(function() {
-        window.location = 'https://steemit.com/@kostas';
-        term.prompt();
-    }, 750);
-}, ['steem']);
+main.register(function (term, command) {
+  term.writeLine('Redirecting to Twitter...')
+  setTimeout(function () {
+    window.location = 'https://twitter.com/kwst_f'
+    term.prompt()
+  }, 750)
+}, ['twitter'])
 
-main.register(function(term, command) {
-    term.writeLine('Redirecting to AngelList...');
-    setTimeout(function() {
-        window.location = 'https://angel.co/konstantinos-faliagkas';
-        term.prompt();
-    }, 750);
-}, ['angellist']);
+main.register(function (term, command) {
+  term.writeLine('Redirecting to Steem...')
+  setTimeout(function () {
+    window.location = 'https://steemit.com/@kostas'
+    term.prompt()
+  }, 750)
+}, ['steem'])
 
-main.register(function(term, command) {
-    term.writeLine('Redirecting to the mailing list subscription page...');
-    setTimeout(function() {
-        window.location = 'https://utlists.utexas.edu/sympa/subscribe/isss';
-        term.prompt();
-    }, 750);
-}, ['mail', 'sub', 'subscribe']);
+main.register(function (term, command) {
+  term.writeLine('Redirecting to AngelList...')
+  setTimeout(function () {
+    window.location = 'https://angel.co/konstantinos-faliagkas'
+    term.prompt()
+  }, 750)
+}, ['angellist'])
 
-main.register(function(term, command) {
-	term.writeLine('/home/kostas');
-	term.prompt();
-}, 'pwd');
+main.register(function (term, command) {
+  term.writeLine('Redirecting to the mailing list subscription page...')
+  setTimeout(function () {
+    window.location = 'https://utlists.utexas.edu/sympa/subscribe/isss'
+    term.prompt()
+  }, 750)
+}, ['mail', 'sub', 'subscribe'])
 
-main.register(function(term, command) {
-	term.element.innerHTML = '';
-	term.prompt();
-}, 'clear');
+main.register(function (term, command) {
+  term.writeLine('/home/kostas')
+  term.prompt()
+}, 'pwd')
 
-main.register(function(term, command) {
-    term.writeLine('This user is not in the cders file. This incident has been reported.');
-    term.prompt();
-}, 'cd');
+main.register(function (term, command) {
+  term.element.innerHTML = ''
+  term.prompt()
+}, 'clear')
 
-main.register(function(term, command) {
-    term.writeLine('guest');
-    term.prompt();
-}, 'whoami');
+main.register(function (term, command) {
+  term.writeLine('This user is not in the cders file. This incident has been reported.')
+  term.prompt()
+}, 'cd')
 
-main.register(function(term, command) {
-    term.writeLine(command.split(' ').slice(1).join(' '));
-    term.prompt();
-}, 'echo');
+main.register(function (term, command) {
+  term.writeLine('guest')
+  term.prompt()
+}, 'whoami')
 
-main.register(function(term, command) {
-    var contentWrapper = document.getElementById('contentWrapper');
-    contentWrapper.classList.toggle('hacker');
-    term.writeLine('Hacker mode: ' + (contentWrapper.classList.contains('hacker') ? 'ENABLED' : 'DISABLED'));
-    term.prompt();
-}, ['hack', 'hacker']);
+main.register(function (term, command) {
+  term.writeLine(command.split(' ').slice(1).join(' '))
+  term.prompt()
+}, 'echo')
 
-main.prompt();
-main.autoType('cat welcome.txt', 1000);
+main.register(function (term, command) {
+  var contentWrapper = document.getElementById('contentWrapper')
+  contentWrapper.classList.toggle('hacker')
+  term.writeLine('Hacker mode: ' + (contentWrapper.classList.contains('hacker') ? 'ENABLED' : 'DISABLED'))
+  term.prompt()
+}, ['hack', 'hacker'])
+
+main.prompt()
+main.autoType('cat welcome.txt', 1000)
